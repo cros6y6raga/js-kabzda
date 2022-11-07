670a7f2fa192f9a49ec2df681cd6e1ed7316d394
cros6y6raga <crosbybraga@gmail.com>
2022-11-07 11:26:16 +0300

    03 - JS/TS for students - functions - 2

--- src/02/02_02.ts
+++ src/02/02_02.ts
@@ -1,25 +1,25 @@
-export type StreetType={
-    title:string
+export type StreetType = {
+    title: string
 }
-export type AddressType={
-    number?:number
+export type AddressType = {
+    number?: number
     street: StreetType
 }
-export type HouseType={
-    buildedAt:number
-    repaired:false
+export type HouseType = {
+    buildedAt: number
+    repaired: boolean
     address: AddressType
 
 }
 export type GovernmentBuildingType = {
     type: "HOSPITAL" | "FIRE-STATION"
-    budget:number
-    staffCount:number
+    budget: number
+    staffCount: number
     address: AddressType
 }
 export type CityType = {
-    title:string
-    houses:Array<HouseType>
-    governmentBuildings:Array<GovernmentBuildingType>
-    citizensNumber:number
+    title: string
+    houses: Array<HouseType>
+    governmentBuildings: Array<GovernmentBuildingType>
+    citizensNumber: number
 }
