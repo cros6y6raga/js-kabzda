6c6c822959d30f3fd905544c841cf04a8cb34512
cros6y6raga <crosbybraga@gmail.com>
2022-11-19 17:35:55 +0300

    05 - JS/TS for students - map

---
+++ src/05/05_01.test.ts
@@ -0,0 +1,18 @@
+import {ManType} from "./05_01";
+
+let people: Array<ManType> = [];
+
+beforeEach(() => {
+    people = [
+        {name: 'Andrew Ivanov', age: 33},
+        {name: 'Alexander Petrov', age: 24},
+        {name: 'Dmitry Sidorov', age: 18},
+    ]
+})
+test('should get array of greeting messages', () => {
+    const messages = people.map(man => `Hello ${man.name.split('')[0]}. Welcom to IT-Incubator`)
+    expect(messages.length).toBe(3)
+    expect(messages[0]).toBe('Hello Andrew. Welcom to IT-Incubator')
+    expect(messages[1]).toBe('Hello Alexander. Welcom to IT-Incubator')
+    expect(messages[2]).toBe('Hello Dmitry. Welcom to IT-Incubator')
+})
